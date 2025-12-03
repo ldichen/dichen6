8c99099966094100d271b75746300a03943034a3
ldichen <ldicccccc@gmail.com>
2025-12-03 21:12:43 +0800

    init blog 251203 6

--- dichen6/src/pages/Home.tsx
+++ dichen6/src/pages/Home.tsx
@@ -2,11 +2,11 @@
  * @Author: DiChen
  * @Date: 2025-12-02 02:51:14
  * @LastEditors: DiChen
- * @LastEditTime: 2025-12-03 17:09:23
+ * @LastEditTime: 2025-12-03 21:12:35
  */
 import React from "react";
 import { Hero } from "../components/home/Hero";
-import { WritingsSection } from "../components/home/WritingsSection";
+// import { WritingsSection } from "../components/home/WritingsSection";
 
 export const Home: React.FC = () => {
   return (
